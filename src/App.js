83a2f67e09d8396fe44437b63735796663694217
FalakNaaz <falucknaz@gmail.com>
2022-08-09 13:51:43 +0530

    login/signup ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import NotFound from './pages/404NotFound/NotFound';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 const Routing = () => {
   return (
     <Routes>
         <Route path="" element={<NotFound/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
     </Routes>
+
   )
 }
 
