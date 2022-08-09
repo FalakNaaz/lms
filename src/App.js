14c110057e58648e35f1826023ab19282ea47403
Pranshu <pranshup23@gmail.com>
2022-08-09 13:14:36 +0530

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <Router>
-        <Routing/>
-    </Router>
+    <Header />
   );
 }
 
