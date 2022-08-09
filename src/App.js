a92ac2d39f602f36b967720a51edc4ed59d9108a
abhiroopGL <abhiroop.panchal@globallogic.com>
2022-08-09 13:32:57 +0530

    Header & Sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './pages/404NotFound/NotFound';
+import Header from './components/Header/Header';
+import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 const Routing = () => {
   return (
     <Routes>
-        <Route path="" element={<NotFound/>}/>
+      <Route path="/Login" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
 
 function App() {
   return (
-    <Router>
-        <Routing/>
-    </Router>
+    <div>
+      <Router>
+        <Header />
+        <Routing />
+      </Router>
+    </div>
   );
 }
 
