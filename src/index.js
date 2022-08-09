e03b535ac093f3bb6bb456675b7f787eca70ee06
Pranshu <pranshup23@gmail.com>
2022-08-09 16:44:45 +0530

    fixing redux part

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import { legacy_createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import AllReducers from './redux/combinedReducer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const store = legacy_createStore(AllReducers, {}, applyMiddleware(thunk));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/combinedReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import Reducers from './reducers/Reducers';
-
-const AllReducers = combineReducers({
-    reducer:Reducers,
-})
-
-export default AllReducers;
