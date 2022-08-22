cf501d45a8c6b325acfa7b9b21142ab39367f0d5
Pranshu <pranshup23@gmail.com>
2022-08-22 12:28:27 +0530

    fix

--- src/components/PrivateRoute/StudentRoute.js
+++ src/components/PrivateRoute/StudentRoute.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 
 function StudentRoute({ children }) {
-    const user = localStorage.getItem("currUserRole");
-    const navigate = useNavigate();
+  const user = localStorage.getItem("currUserRole");
+  const navigate = useNavigate();
 
+  if (user) {
     if (user && user === "Learner") {
-        return children
-    }else{
-        setTimeout(()=>{
-            navigate(-1);
-        }, 1)
+      return children;
+    } else {
+      setTimeout(() => {
+        navigate(-1);
+      }, 1);
     }
+  }
+  return <Navigate to="/login" />;
 }
-export default StudentRoute
+export default StudentRoute;
--- src/components/PrivateRoute/TrainerRoute.js
+++ src/components/PrivateRoute/TrainerRoute.js
@@ -1,15 +1,17 @@
-
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate } from "react-router-dom";
 
 function TrainerRoute({ children }) {
-    const user = localStorage.getItem("currUserRole");
-    const navigate = useNavigate();
+  const user = localStorage.getItem("currUserRole");
+  const navigate = useNavigate();
+  if (user) {
     if (user === "Trainer") {
-        return children
-    }else{
-        setTimeout(()=>{
-            navigate(-1);
-        }, 1)
+      return children;
+    } else {
+      setTimeout(() => {
+        navigate(-1);
+      }, 1);
     }
+  }
+  return <Navigate to="/login"/>
 }
-export default TrainerRoute
+export default TrainerRoute;
