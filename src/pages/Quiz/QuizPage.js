4dd8eceb4684a16d485fc8651a8f1e432d477852
suraj2k <suraj.chaurasia@globallogic.com>
2022-08-23 15:14:37 +0530

    css added

--- src/pages/Quiz/QuizPage.js
+++ src/pages/Quiz/QuizPage.js
@@ -44,7 +44,7 @@ export default function App() {
     console.log("score = ", score);
   }
 	return (
-		<div className='app'>
+		<div className='app-quiz'>
 			{showScore ? (
 				<div className='score-section'>
 					You scored {score} out of {questions.length}
